e134e634805aebabb2b8894e4ab8f1f31a887064
Ihfadzh D <ihfadzhdiinaka7@gmail.com>
2025-02-21 11:41:30 +0700

    firs commit

--- index.php
+++ index.php
@@ -1,1 +1,50 @@
-<img src="generate_barcode.php" alt="Scan untuk masuk" />
+<!DOCTYPE html>
+<html>
+
+<head>
+  <!-- Basic -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+
+  <title>Cuy Absen</title>
+
+  <!-- slider stylesheet -->
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+  <!-- bootstrap core css -->
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+  <!-- font awesome style -->
+  <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" />
+
+  <!-- Custom styles for this template -->
+  <link href="css/style.css" rel="stylesheet" />
+  <!-- responsive style -->
+  <link href="css/responsive.css" rel="stylesheet" />
+  <script>
+    window.onpopstate = function () {
+    document.getElementById("myModal").style.display = "block";
+};
+  </script>
+</head>
+
+<body>
+    <!-- slider section -->
+    <section class="slider_section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="detail-box">
+            <img src="generate_barcode.php" alt="Scan untuk masuk" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+</body>
+
+</html>
